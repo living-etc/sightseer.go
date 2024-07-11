a54e8dfcc92ac75b8a337a159b931b625f0457b9
Chris Wilson <hey@chris-wilson.me>
2024-07-11 11:21:11 +0100

    Update service test to handle preset/vendor preset
    There are two possible strings that can be found in systemctl output
    indicating whether or not the unit starts automatically after
    installation - "preset" and "vendor preset". I'm not currently sure
    about the difference between the two, but currently I'm assuming
    "preset" is found on pre-installed units and "vendor preset" is for
    user-installed units.

--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
 # TODO
 
-- Test error handling - getting it sorted this once should mean it's mostly
-  sorted for everything else we do in the future
 - Add an etcdctl command executor to the kubernetes client and refactor the test
   for the etcd cluster members
 - Document things, but wait a bit until the design has settled down a bit -
   things are chruning a lot right now
+- Research the scheme for systemctl output. The difference between "preset" and
+  "vendor preset" tripped me up - ssh service only had preset, but kubelet had
+  "vendor preset".
--- ssh/service.go
+++ ssh/service.go
@@ -5,25 +5,25 @@ import (
 )
 
 type Service struct {
-	Active       string
-	Enabled      string
-	Loaded       string
-	UnitFile     string
-	VendorPreset string
+	Active   string
+	Enabled  string
+	Loaded   string
+	UnitFile string
+	Preset   string
 }
 
 func serviceFromSystemctl(systemctlOutput string) (Service, error) {
-	pattern := `Loaded: (?P<Loaded>\w+) \((?P<UnitFile>.*?); (?P<Enabled>\w+); vendor preset: (?P<VendorPreset>.*?)\)\s+Active: (?P<Active>.*? \(.+?\))`
+	pattern := `Loaded: (?P<Loaded>\w+) \((?P<UnitFile>.*?); (?P<Enabled>\w+); .*: (?P<Preset>.*?)\)\s+Active: (?P<Active>.*? \(.+?\))`
 
 	re := regexp.MustCompile(pattern)
 	matches := re.FindStringSubmatch(systemctlOutput)
 
 	service := Service{
-		Active:       matches[re.SubexpIndex("Active")],
-		Enabled:      matches[re.SubexpIndex("Enabled")],
-		Loaded:       matches[re.SubexpIndex("Loaded")],
-		UnitFile:     matches[re.SubexpIndex("UnitFile")],
-		VendorPreset: matches[re.SubexpIndex("VendorPreset")],
+		Active:   matches[re.SubexpIndex("Active")],
+		Enabled:  matches[re.SubexpIndex("Enabled")],
+		Loaded:   matches[re.SubexpIndex("Loaded")],
+		UnitFile: matches[re.SubexpIndex("UnitFile")],
+		Preset:   matches[re.SubexpIndex("Preset")],
 	}
 
 	return service, nil
