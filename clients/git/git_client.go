a276b0da99c86eedc56723b73af3ab6079a5724c
Chris Wilson <hey@chris-wilson.me>
2025-01-05 18:30:54 +0000

    Build git client tests around actual repo

--- clients/git/git_client.go
+++ clients/git/git_client.go
@@ -1,6 +1,9 @@
 package git
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -13,24 +16,136 @@ func NewGitClient(path string) GitClient {
 	return GitClient{path: path}
 }
 
-type Ref struct {
-	Subject string
+type Commit struct {
+	Hash                           string `json:"Hash"`
+	AbbreviatedHash                string `json:"AbbreviatedHash"`
+	TreeHash                       string `json:"TreeHash"`
+	AbbreviatedTreeHash            string `json:"AbbreviatedTreeHash"`
+	ParentHashes                   string `json:"ParentHashes"`
+	AbbreviatedTree                string `json:"AbbreviatedTree"`
+	AuthorName                     string `json:"AuthorName"`
+	AuthorNameMailmap              string `json:"AuthorNameMailmap"`
+	AuthorEmail                    string `json:"AuthorEmail"`
+	AuthorEmailMailmap             string `json:"AuthorEmailMailmap"`
+	AuthorEmailLocalPart           string `json:"AuthorEmailLocalPart"`
+	AuthorEmailLocalPartMailmap    string `json:"AuthorEmailLocalPartMailmap"`
+	AuthorDate                     string `json:"AuthorDate"`
+	AuthorDateRFC2822              string `json:"AuthorDateRFC2822"`
+	AuthorDateUNIX                 string `json:"AuthorDateUNIX"`
+	AuthorDateISO8601Like          string `json:"AuthorDateISO8601Like"`
+	AuthorDateISO8601Strict        string `json:"AuthorDateISO8601Strict"`
+	AuthorDateShort                string `json:"AuthorDateShort"`
+	CommitterName                  string `json:"CommitterName"`
+	CommitterNameMailmap           string `json:"CommitterNameMailmap"`
+	CommitterEmail                 string `json:"CommitterEmail"`
+	CommitterEmailMailmap          string `json:"CommitterEmailMailmap"`
+	CommitterEmailLocalPart        string `json:"CommitterEmailLocalPart"`
+	CommitterEmailLocalPartMailmap string `json:"CommitterEmailLocalPartMailmap"`
+	CommitterDate                  string `json:"CommitterDate"`
+	CommitterDateRFC2822           string `json:"CommitterDateRFC2822"`
+	CommitterDateUNIX              string `json:"CommitterDateUNIX"`
+	CommitterDateISO8601Like       string `json:"CommitterDateISO8601Like"`
+	CommitterDateISO8601Strict     string `json:"CommitterDateISO8601Strict"`
+	CommitterDateShort             string `json:"CommitterDateShort"`
+	RefNames                       string `json:"RefNames"`
+	RefNamesUnwrapped              string `json:"RefNamesUnwrapped"`
+	Encoding                       string `json:"Encoding"`
+	Subject                        string `json:"Subject"`
+	SubjectSanitised               string `json:"SubjectSanitised"`
+	Body                           string `json:"Body"`
+	CommitNotes                    string `json:"CommitNotes"`
+	VerificationMessageRaw         string `json:"VerificationMessageRaw"`
+	SignerName                     string `json:"SignerName"`
+	SigningKey                     string `json:"SigningKey"`
+	SigningKeyFingerprint          string `json:"SigningKeyFingerprint"`
+	SigningKeyTrustLevel           string `json:"SigningKeyTrustLevel"`
+	ReflogSelector                 string `json:"ReflogSelector"`
+	ReflogSelectorShortened        string `json:"ReflogSelectorShortened"`
+	ReflogIdentityName             string `json:"ReflogIdentityName"`
+	ReflogIdentityNameMailmap      string `json:"ReflogIdentityNameMailmap"`
+	ReflogIdentityEmail            string `json:"ReflogIdentityEmail"`
+	ReflogIdentityEmailMailmap     string `json:"ReflogIdentityEmailMailmap"`
+	ReflogSubject                  string `json:"ReflogSubject"`
 }
 
-func (client *GitClient) Ref(identifier string) (*Ref, error) {
-	cmdParts := []string{"git", "show", "-s", "--format=%s", identifier}
+func (client *GitClient) Commit(identifier string) (*Commit, error) {
+	formatParts := []string{
+		"{\"Hash\":\"%H\"",
+		"\"AbbreviatedHash\":\"%h\"",
+		"\"TreeHash\":\"%T\"",
+		"\"AbbreviatedTreeHash\":\"%t\"",
+		"\"ParentHashes\":\"%P\"",
+		"\"AbbreviatedTree\":\"%p\"",
+		"\"AuthorName\":\"%an\"",
+		"\"AuthorNameMailmap\":\"%aN\"",
+		"\"AuthorEmail\":\"%ae\"",
+		"\"AuthorEmailMailmap\":\"%aE\"",
+		"\"AuthorEmailLocalPart\":\"%al\"",
+		"\"AuthorEmailLocalPartMailmap\":\"%aL\"",
+		"\"AuthorDate\":\"%ad\"",
+		"\"AuthorDateRFC2822\":\"%aD\"",
+		"\"AuthorDateUNIX\":\"%at\"",
+		"\"AuthorDateISO8601Like\":\"%ai\"",
+		"\"AuthorDateISO8601Strict\":\"%aI\"",
+		"\"AuthorDateShort\":\"%as\"",
+		"\"AuthorDateHuman\":\"%ah\"",
+		"\"CommitterName\":\"%cn\"",
+		"\"CommitterNameMailmap\":\"%cN\"",
+		"\"CommitterEmail\":\"%ce\"",
+		"\"CommitterEmailMailmap\":\"%cE\"",
+		"\"CommitterEmailLocalPart\":\"%cl\"",
+		"\"CommitterEmailLocalPartMailmap\":\"%cL\"",
+		"\"CommitterDate\":\"%cd\"",
+		"\"CommitterDateRFC2822\":\"%cD\"",
+		"\"CommitterDateUNIX\":\"%ct\"",
+		"\"CommitterDateISO8601Like\":\"%ci\"",
+		"\"CommitterDateISO8601Strict\":\"%cI\"",
+		"\"CommitterDateShort\":\"%cs\"",
+		"\"RefNames\":\"%d\"",
+		"\"RefNamesUnwrapped\":\"%D\"",
+		"\"Encoding\":\"%e\"",
+		"\"Subject\":\"%s\"",
+		"\"SubjectSanitised\":\"%f\"",
+		"\"Body\":\"%b\"",
+		"\"CommitNotes\":\"%N\"",
+		"\"VerificationMessageRaw\":\"%GG\"",
+		"\"SignerName\":\"%GS\"",
+		"\"SigningKey\":\"%GK\"",
+		"\"SigningKeyFingerprint\":\"%GF\"",
+		"\"SigningKeyTrustLevel\":\"%GT\"",
+		"\"ReflogSelector\":\"%gD\"",
+		"\"ReflogSelectorShortened\":\"%gd\"",
+		"\"ReflogIdentityName\":\"%gn\"",
+		"\"ReflogIdentityNameMailmap\":\"%gN\"",
+		"\"ReflogIdentityEmail\":\"%ge\"",
+		"\"ReflogIdentityEmailMailmap\":\"%gE\"",
+		"\"ReflogSubject\":\"%gs\"}",
+	}
+
+	cmdParts := []string{
+		"git",
+		"show",
+		"--no-patch",
+		fmt.Sprintf("--format=%s", strings.Join(formatParts, ",")),
+		identifier,
+	}
 	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
 	cmd.Dir = client.path
 
-	var out strings.Builder
-	cmd.Stdout = &out
+	var stdout, stderr strings.Builder
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
 	err := cmd.Run()
+	if err != nil {
+		return nil, errors.New(stderr.String())
+	}
+
+	commit := &Commit{}
+	err = json.Unmarshal([]byte(stdout.String()), &commit)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Ref{
-		Subject: strings.Trim(out.String(), "\n"),
-	}, nil
+	return commit, nil
 }
--- clients/git/git_client_test.go
+++ clients/git/git_client_test.go
@@ -1,39 +1,85 @@
 package git_test
 
 import (
-	"os/exec"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 
 	client "github.com/living-etc/sightseer.go/clients/git"
 )
 
 func TestNewGitClient(t *testing.T) {
-	repoDir := t.TempDir()
-
-	setupCommands := [][]string{
-		{"git", "init"},
-		{"touch", "README.md"},
-		{"git", "add", "README.md"},
-		{"git", "commit", "-m", "Initial Commit"},
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	for _, setupCommand := range setupCommands {
-		cmd := exec.Command(setupCommand[0], setupCommand[1:]...)
-		cmd.Dir = repoDir
+	repoDir := filepath.Join(wd + "/testdata/git-client-test-repo-for-sightseer")
 
-		err := cmd.Run()
-		if err != nil {
-			t.Fatal(err)
-		}
+	commitWant := &client.Commit{
+		Hash:                           "108dd57ebe039833ad45f0e997fb68ad4fe49b6c",
+		AbbreviatedHash:                "108dd57",
+		TreeHash:                       "f93e3a1a1525fb5b91020da86e44810c87a2d7bc",
+		AbbreviatedTreeHash:            "f93e3a1",
+		ParentHashes:                   "",
+		AbbreviatedTree:                "",
+		AuthorName:                     "Sightseer Test User",
+		AuthorNameMailmap:              "Sightseer Test User",
+		AuthorEmail:                    "sightseer@example.com",
+		AuthorEmailMailmap:             "sightseer@example.com",
+		AuthorEmailLocalPart:           "sightseer",
+		AuthorEmailLocalPartMailmap:    "sightseer",
+		AuthorDate:                     "Sun Jan 5 16:47:26 2025 +0000",
+		AuthorDateRFC2822:              "Sun, 5 Jan 2025 16:47:26 +0000",
+		AuthorDateUNIX:                 "1736095646",
+		AuthorDateISO8601Like:          "2025-01-05 16:47:26 +0000",
+		AuthorDateISO8601Strict:        "2025-01-05T16:47:26+00:00",
+		AuthorDateShort:                "2025-01-05",
+		CommitterName:                  "Sightseer Test User",
+		CommitterNameMailmap:           "Sightseer Test User",
+		CommitterEmail:                 "sightseer@example.com",
+		CommitterEmailMailmap:          "sightseer@example.com",
+		CommitterEmailLocalPart:        "sightseer",
+		CommitterEmailLocalPartMailmap: "sightseer",
+		CommitterDate:                  "Sun Jan 5 16:47:26 2025 +0000",
+		CommitterDateRFC2822:           "Sun, 5 Jan 2025 16:47:26 +0000",
+		CommitterDateUNIX:              "1736095646",
+		CommitterDateISO8601Like:       "2025-01-05 16:47:26 +0000",
+		CommitterDateISO8601Strict:     "2025-01-05T16:47:26+00:00",
+		CommitterDateShort:             "2025-01-05",
+		RefNames:                       " (HEAD -> main, origin/main, origin/HEAD)",
+		RefNamesUnwrapped:              "HEAD -> main, origin/main, origin/HEAD",
+		Encoding:                       "",
+		Subject:                        "Initial Commit",
+		SubjectSanitised:               "Initial-Commit",
+		Body:                           "",
+		CommitNotes:                    "",
+		VerificationMessageRaw:         "",
+		SignerName:                     "",
+		SigningKey:                     "",
+		SigningKeyFingerprint:          "",
+		SigningKeyTrustLevel:           "undefined",
+		ReflogSelector:                 "",
+		ReflogSelectorShortened:        "",
+		ReflogIdentityName:             "",
+		ReflogIdentityNameMailmap:      "",
+		ReflogIdentityEmail:            "",
+		ReflogIdentityEmailMailmap:     "",
+		ReflogSubject:                  "",
 	}
 
 	t.Run("new client", func(t *testing.T) {
 		gitClient := client.NewGitClient(repoDir)
 
-		head, _ := gitClient.Ref("main")
+		head, err := gitClient.Commit("main")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-		if head.Subject != "Initial Commit" {
-			t.Errorf("want %v, got %v,", "Initial Commit", head.Subject)
+		if diff := cmp.Diff(commitWant, head); diff != "" {
+			t.Error(diff)
 		}
 	})
 }
